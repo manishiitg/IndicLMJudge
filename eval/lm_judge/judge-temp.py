ab6ccd8bc7ac5eda940d60fb327cf019b8d86543
Manish Prakash <manish.prakash@citymall.live>
2024-02-28 07:55:28 +0530

    initial commit

--- eval/lm_judge/judge-temp.py
+++ eval/lm_judge/judge-temp.py
@@ -1,18 +1,12 @@
 from tqdm import tqdm
 import argparse
 import os
-import random
 import torch
 import json
 from datasets import load_dataset
-from eval.utils import (
-    dynamic_import_function,
-)
-from huggingface_hub import HfApi
 from transformers import AutoTokenizer
 import vllm
 from datasets import Dataset
-from datetime import date
 import torch
 import re
 
