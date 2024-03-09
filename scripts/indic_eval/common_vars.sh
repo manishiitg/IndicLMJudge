6291b5fb32510a4a1eb0f597a5ef15d933fa2a54
Manish Prakash <manish.prakash@citymall.live>
2024-03-09 14:05:51 +0530

    initial commit

--- scripts/indic_eval/common_vars.sh
+++ scripts/indic_eval/common_vars.sh
@@ -13,7 +13,8 @@ model_names=(
     # "manishiitg/open-aditi-hi-v2"
     # "manishiitg/open-aditi-hi-v1"   
     # "sarvamai/OpenHathi-7B-Hi-v0.1-Base"
-    "manishiitg/open-aditi-hi-v3-dpo-awq"
+    # "manishiitg/open-aditi-hi-v3-dpo-awq"
     # "manishiitg/open-aditi-hi-v5"
+    "manishiitg/open-aditi-chat-hi-1.91-awq"
 )
 FOLDER_BASE=/sky-notebook/eval-results
