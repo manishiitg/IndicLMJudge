1f082fb9c61d6afe387be68cbb973e476c723901
Manish Prakash <manish.prakash@citymall.live>
2024-03-20 11:05:33 +0530

    updates

--- scripts/indic_eval/lmjudge.sh
+++ scripts/indic_eval/lmjudge.sh
@@ -26,6 +26,9 @@ for model_name_or_path in "${model_names[@]}"; do
     if echo "$model_name" | grep -qi "OpenHathi-7B-Hi-v0.1-Base"; then
         template_format="eval.templates.create_prompt_with_llama2_chat_format"
     fi
+    if echo "$model_name" | grep -qi "OpenHermes"; then
+        template_format="eval.templates.create_prompt_with_chatml_format"
+    fi
     if [ "$check_file_existence" = false ] || [ ! -f "$FILE" ]; then
         python3 -m eval.lm_judge.run_eval \
             --save_dir $FOLDER \
