f75bcb181db201f4dc29d5b49700f06a513fcfb8
Manish Prakash <manish.prakash@citymall.live>
2024-04-03 12:01:58 +0530

    updates

--- eval/lm_judge/judge.py
+++ eval/lm_judge/judge.py
@@ -165,7 +165,7 @@ def eval_hf_model(args, model, tokenizer, prompts):
 def main(args):
 
     ds = load_dataset("manishiitg/llm_judge", split="train")
-    ds = ds.filter(lambda x: x["model_name"] == "google/gemma-2b-it").filter(lambda x: x["lang"] == "hi").shuffle(range(10))
+    ds = ds.filter(lambda x: x["model_name"] == "google/gemma-2b-it").filter(lambda x: x["lang"] == "hi").shuffle().select(range(10))
     final_data = []
     for row in ds:
         final_data.append(row)
